234d92a33132d1dac0a588985d9d676a41cc5bf7
Daniel Moerner <dmoerner@gmail.com>
2016-11-23 10:31:46 -0500

    Clean up documentation and update for denanomyizer.bash

--- anonymizer.bash
+++ anonymizer.bash
@@ -4,68 +4,58 @@
 #
 # Students have uploaded their assignments to their Dropbox on
 # Classesv2. The files themselves are assumed to contain no
-# identifying characteristics of the students. (I ask them to not put
-# their names in the files.) The script makes no assumptions about the
-# names of the files themselves; we will rename them without the TF
-# ever seeing the original names. The script assumes each Dropbox has
-# only a single file in it. We only allow them to upload doc, docx, or
-# pdf assignments.
+# identifying characteristics of the students. I ask them to not put
+# their names in the files. The script itself will rename their files,
+# so it's fine if they put their names in the titles. The script
+# assumes each Dropbox has only a single file in it. We only allow
+# them to upload doc, docx, or pdf assignments.
 
 # The files are correlated with a directory whose name is the
-# student's user ID. We cannot simply use the classesv2 frontend to
-# zip up the files and download them. This renames the directories to
-# a hash which, while unique, varies each time the directory is
-# accessed.
+# student's full name on the web interface.  We cannot simply use the
+# classesv2 frontend to zip up the files and download them. This
+# renames the directories to a hash which, while unique, varies each
+# time the directory is accessed.
 
 # Instead we use curl to access the WebDAV interface remotely and
-# download each file into the directory whose name is the respective
-# student's ID.
+# download each file. We use rot47 to reversibly "encrypt" the name of
+# each file in the format <class>-<assignment>-<student id>. These
+# files are downloaded in a directory with the name
+# <class>-<assignment>-anonymized.
 
-# We then rename each file in each directory to an appropriate
-# hash. (At present I just calculate the md5sum of the student's
-# submitted assignment.)
+# The script is designed to be fully interactive, and prompts the user
+# for their classesv2 link, their username, their password (hidden,
+# but here we have to hope that curl properly implements https access
+# to WebDAV), and the assignment name. It also makes sure that the
+# right number of files are available for download before we proceed.
 
-# We then construct a csv file which can be imported into Excel (or
-# equivalent) and which correlates each Student ID with the
-# appropriate md5sum.
-
-# We finally copy all the student assignments into an output
-# folder. They are now anonymized such that they can be graded by the
-# TF. After grading the TF then correlates the names of the files with
-# the student IDs given in the csv file.
+# This script is coupled with deanonymizer.bash, which takes as its
+# only argument the name of the <class>-<assignment>-anonymized
+# directory, and then reverses the rot47 procedure to generate a new
+# directory with the deanonymized files.
 
 # I should add that this is presently a total hackjob.
 
 # TODO:
 #
 # 1. Implement a command-line option to use MAT to strip the files of
-# identifying metadata. Ideally for security reasons we would also
-# implement an option to clean pdf files along the lines described
-# here:
-# https://blog.invisiblethings.org/2013/02/21/converting-untrusted-pdfs-into-trusted.html
-# Obviously as it stands this security measure is not portable.
+# identifying metadata.
 #
-# 2. Implement another script which de-anonymizes the files after
-# grading is complete. This will facilitate returning any files that
-# may have comments on them. Really what we should do is not calculate
-# the md5 of the original file but instead try to generate some sort
-# of a hash from their student IDs themselves, that way we can then
-# just reverse the operation without saving any csv file at all.
+# 2. At present, 2-factor authentication is not supported. (There's a
+# purported guide available, but I don't think this works with Yale:
+# http://www.qed42.com/blog/using-curl-webdav-two-factor-authentication)
+# The workaround is to make sure you have already logged in to
+# classesv2 and have checked to stay logged in for a day, or to run
+# this script on Yale campus.
 #
-# 3. I am unsure if there is also a way to correlate student IDs with
-# email addresses. At present this must be manually done by the TF by
-# looking at the Roster on classesv2.
+# 3. At present, the best we can do is output the files with the
+# student's uid. It would be great to correlate uid with email
+# addresses automatically. I can't figure out how to do that yet.
 
-# We should be able to generate this automatically so the user doesn't
-# have to get it from the "upload/download multiple resources" tab of
-# the Dropbox.
-CLASSESV2="https://classesv2.yale.edu/dav/group-user/gman213_f16"
-
-# I'll pretend to have a safe implementation of temporary directories.
-tempdir=$(mktemp -dt "$(basename $0).XXXXXXXXXX")
-tempprop=$tempdir/propfind
-# This should be implemented better.
-CSV=$tempdir/"csv"
+# We'll work in a temporary directory to start:
+TEMPDIR=$(mktemp -dt "$(basename $0).XXXXXXXXXX")
+# This file will store the propfind data we will parse to know what to
+# download.
+TEMPPROP=$TEMPDIR/propfind
 
 # First we need to download everything
 
@@ -74,9 +64,11 @@ CSV=$tempdir/"csv"
 # which should work, has trouble accessing the hashed directory names
 # used in classesv2's implementation of WebDAV.
 
-# The hacky solution is to first use PROPFIND to get the proper
-# directory structure from classesv2, then parse it and recursively
-# download all the assignments.
+# The solution is to use curl twice. First use PROPFIND to get the
+# proper directory structure from classesv2, then parse it and
+# recursively download all the assignments.
+
+# Spit out some warning messages first:
 echo "This does not yet support two-factor authentication."
 echo "Make sure you are on Yale's campus or have already accepted a push."
 
@@ -103,10 +95,10 @@ read -s PWD
 echo "Using curl to fetch classesv2 directory structure..."
 curl -u "$TFUSER":"$PWD" -i -X PROPFIND "$CV2WEBDAV" \
      --data "<D:propfind xmlns:D='DAV:'><D:prop><D:response/></D:prop></D:propfind>" \
-    | grep -e doc -e pdf > $tempprop
+    | grep -e doc -e pdf > $TEMPPROP
 
 # Verify that the expected number of assignments have been received.
-echo "$(wc -l $tempprop | awk '{print $1}') files found. Type 'yes' to continue"
+echo "$(wc -l $TEMPPROP | awk '{print $1}') files found. Type 'yes' to continue"
 read reply
 if [[ ! "$reply" = "yes" ]]; then
     echo "Exiting, check if all assignments have been submitted."
@@ -114,43 +106,42 @@ if [[ ! "$reply" = "yes" ]]; then
 fi
 
 # Ask the user for a way to describe the assignment
-echo "Please enter a one-word name for the assignment (e.g., assignment-2)"
+echo "Please enter a very short descrption for the assignment (e.g., 'Assignment 2')"
 read ASSIGNMENTNAME
 
 # We now loop through each line of the propfind file. For each line,
 # we parse it to generate three variables: the full name to be
-# downloaded by curl, the student ID. Then we download each file into
-# the appropriate directory. Then we calculate a hash of the files and
-# store that in a variable. Then we append the student ID and the hash
-# to the csv file. Finally we rename the file and move it to the final
-# directory.
+# downloaded by curl, the student ID, and the filename of their
+# submission. We give each file an unobfuscated name that reflects the
+# class, assignment, and student id. Then we use rot47 to calculate
+# the obfuscated name of the file. Then we download each file into the
+# temporary directory under its obfuscated name. 
 
 while read line; do
 # Even if the students put one of our delimiters [<>] into their
 # filename, it seems that WebDAV automatically replaces it with an
 # underscore.
-    davaddr="https://classesv2.yale.edu$(echo "$line" | awk -F '[<>]' '{print $5}')"
-    sid=$(echo $davaddr | awk -F '/' '{print $(NF-1)}')
-    filename=$(basename $davaddr)
+    DAVADDR="https://classesv2.yale.edu$(echo "$line" | awk -F '[<>]' '{print $5}')"
+    SID=$(echo $DAVADDR | awk -F '/' '{print $(NF-1)}')
+    FILENAME=$(basename $DAVADDR)
 
     # Unobfuscated name
-    UNOBNAME="$CLASS-$ASSIGNMENTNAME-$sid"
+    UNOBNAME="$CLASS-$ASSIGNMENTNAME-$SID"
     # Obfuscated name (Using ROT47)
-    OBNAME="$(echo "$UNOBNAME" | tr '\!-~' 'P-~\!-O')"."${filename##*.}"
+    OBNAME="$(echo "$UNOBNAME" | tr '\!-~' 'P-~\!-O')"."${FILENAME##*.}"
 
     # Download the files. I wish there were a less resource intensive
     # way to do this.
-    curl -u $TFUSER:"$PWD" $davaddr --output $tempdir/"$OBNAME"
+    curl -u "$TFUSER":"$PWD" "$DAVADDR" --output "$TEMPDIR"/"$OBNAME"
 
-    # This will be replaced momentarily
-    echo "$UNOBNAME,$OBNAME" >> $CSV
-    
-done < $tempprop
+done < "$TEMPPROP"
 
-# Move the files back into the user's directory:
+# Finally we move the files into the anonymized directory and clean up:
 OUTPUTDIR="$CLASS-$ASSIGNMENTNAME-anonymized"
-rm $tempprop
+rm $TEMPPROP
 mkdir -p "$OUTPUTDIR"
-mv $tempdir/* "$OUTPUTDIR"
-rmdir $tempdir
+mv $TEMPDIR/* "$OUTPUTDIR"
+rmdir $TEMPDIR
 
+echo "Anonymization complete!"
+echo "Run "deanonymizer.bash "$OUTPUTDIR"" when you are done grading!"
