6251ee42b3c46e5e1c1169302d82c5e0a7f37518
Daniel Moerner <dmoerner@gmail.com>
2016-11-25 18:17:27 -0500

    Implement section-by-section anonymizing

--- anonymizer.bash
+++ anonymizer.bash
@@ -1,39 +1,55 @@
 #!/bin/bash
 
-# This script is designed for the following use case:
+# This script is (c) 2016 Daniel Moerner <dmoerner@gmail.com> and
+# licensed under the MIT License.
 #
-# Students have uploaded their assignments to their Dropbox on
-# Classesv2. The files themselves are assumed to contain no
-# identifying characteristics of the students. I ask them to not put
-# their names in the files. The script itself will rename their files,
-# so it's fine if they put their names in the titles. The script
-# assumes each Dropbox has only a single file in it. We only allow
-# them to upload doc, docx, or pdf assignments.
-
-# The files are correlated with a directory whose name is the
-# student's full name on the web interface.  We cannot simply use the
-# classesv2 frontend to zip up the files and download them. This
-# renames the directories to a hash which, while unique, varies each
-# time the directory is accessed.
-
-# Instead we use curl to access the WebDAV interface remotely and
-# download each file. We use rot47 to reversibly "encrypt" the name of
-# each file in the format <class>-<assignment>-<student id>. These
-# files are downloaded in a directory with the name
-# <class>-<assignment>-anonymized.
-
-# The script is designed to be fully interactive, and prompts the user
-# for their classesv2 link, their username, their password (hidden,
-# but here we have to hope that curl properly implements https access
-# to WebDAV), and the assignment name. It also makes sure that the
-# right number of files are available for download before we proceed.
+# This script is designed to anonymize student submissions to
+# classesv2. It is entirely interactive and explains to the user what
+# they need to provide.
+#
+# The script requires bash, curl, and awk.
+#
+# The script assumes the following:
+#
+# 1. The students have uploaded a submission in doc, docx, or pdf to
+# their Dropbox on classesv2. This is the only file in their Dropbox.
+#
+# 2. The students themselves have not included their names in the
+# documents. (Whether or not they put their name in the filename is
+# irrelevant because we will rename all files. We don't bother trying
+# to strip metadata, especially since MAT is currently broken for
+# pdfs: https://0xacab.org/mat/mat/issues/11067.)
+#
+# The script runs in two modes: "all" and "section".
+#
+# The "all" mode anonymizes all submissions to the classesv2
+# Dropboxes. This is appropriate when there is only a single TF, or
+# when TFs agree in advance to all anonymously grade each others'
+# work.
+#
+# The "section" mode anonymizes section by section. This is not fully
+# automated because of weaknesses in the WebDAV interface. The TF has
+# to manually download a roster file from classesv2. The script loops
+# through user-provided roster files, putting anonymized files for
+# each section in their respective directories.
+#
+# We maintain anonymity by using rot47 encryption. (This is hopefully
+# more obscure than mere rot13 but this something that might need to
+# be explored in the future. It requires a certain lack of attention
+# from the TF.)
 
 # This script is coupled with deanonymizer.bash, which takes as its
-# only argument the name of the <class>-<assignment>-anonymized
-# directory, and then reverses the rot47 procedure to generate a new
-# directory with the deanonymized files.
+# only argument the name of the anonymized directory, and then
+# reverses the rot47 procedure to generate a new directory with the
+# deanonymized files.
 
 # I should add that this is presently a total hackjob.
+
+# Error handling:
+# exit 1 = we got some user input that wasn't "yes" or ne of the options.
+# exit 2 = some file we expected isn't present
+# exit 3 = catastrophe has occurred (i.e., something we already
+#          error-checked has recurred somehow)
 
 # We'll work in a temporary directory to start:
 TEMPDIR=$(mktemp -dt "$(basename $0).XXXXXXXXXX")
@@ -41,7 +57,7 @@ TEMPDIR=$(mktemp -dt "$(basename $0).XXXXXXXXXX")
 # download.
 TEMPPROP=$TEMPDIR/propfind
 
-# First we need to download everything
+# First we need to download everything.
 
 # Cadaver, which is a command-line tool to interact with WebDAV, does
 # not play nicely with downloading files in directories. Wget -r,
@@ -53,8 +69,11 @@ TEMPPROP=$TEMPDIR/propfind
 # recursively download all the assignments.
 
 # Spit out some warning messages first:
-echo "This does not yet support two-factor authentication."
+echo "Welcome to blind-grader! Feel free to contact dmoerner@gmail.com"
+echo
+echo "Warning: This does not yet support two-factor authentication."
 echo "Make sure you are on Yale's campus or have already accepted a push."
+echo
 
 # We extract CLASS and the WebDav interface from their classesv2
 # link. We will use the CLASS variable independently to organize files
@@ -84,18 +103,41 @@ curl -u "$TFUSER":"$PWD" -i -X PROPFIND "$CV2WEBDAV" \
 # Verify that the expected number of assignments have been
 # received. Add warning that this downloads files for all TFs.
 echo
-echo "Warning: This script will download and anonymize the files for all sections."
-echo "If you are only trying to anonymize some sections, this will not work." 
-echo "$(wc -l $TEMPPROP | awk '{print $1}') files found. Type 'yes' to continue"
+echo "$(wc -l $TEMPPROP | awk '{print $1}') total assignments found."
+echo
+echo "You now have the choice to either anonymize all assignments or try to"
+echo "anonymize by section."
+echo
+echo "Warning: Anonymizing by section is not completely automated and is "
+echo "somewhat experimental."
+echo
+echo "Type 'all' to anonymize the whole class, or 'section' to anonymize"
+echo "by section:"
 read reply
-if [[ ! "$reply" = "yes" ]]; then
-    echo "Exiting, check if all assignments have been submitted."
-    exit 1
-fi
+case "$reply" in
+    "all")
+	USE="all"
+	echo "Anonymizing all sections!"
+	;;
+    "section")
+	USE="section"
+	echo "Anonymizing section-by-section!"
+	;;
+    *)
+	echo "Option not recognized, exiting now."
+	exit 1
+	;;
+esac
+
 
 # Ask the user for a way to describe the assignment
-echo "Please enter a very short descrption for the assignment (e.g., 'Assignment 2')"
-read ASSIGNMENTNAME
+echo
+echo "Great! Please enter a name for the assignment:"
+read ASSIGNMENT
+
+echo
+echo "Downloading all files now. This may take a second."
+echo
 
 # We now loop through each line of the propfind file. For each line,
 # we parse it to generate three variables: the full name to be
@@ -114,26 +156,155 @@ while read line; do
     FILENAME=$(basename $DAVADDR)
 
     # Unobfuscated name
-    UNOBNAME="$CLASS-$ASSIGNMENTNAME-$SID"
+    UNOBNAME="$CLASS-$ASSIGNMENT-$SID"
     # Obfuscated name (Using ROT47)
     OBNAME="$(echo "$UNOBNAME" | tr '\!-~' 'P-~\!-O')"."${FILENAME##*.}"
 
     # Download the files. I wish there were a less resource intensive
     # way to do this.
-    curl -u "$TFUSER":"$PWD" "$DAVADDR" --output "$TEMPDIR"/"$OBNAME"
+    curl -s -u "$TFUSER":"$PWD" "$DAVADDR" --output "$TEMPDIR"/"$OBNAME"
 
 done < "$TEMPPROP"
 
-# Finally we move the files into the anonymized directory and clean up:
-OUTPUTDIR="$CLASS-$ASSIGNMENTNAME-anonymized"
-rm $TEMPPROP
-mkdir -p "$OUTPUTDIR"
-mv $TEMPDIR/* "$OUTPUTDIR"
-rmdir $TEMPDIR
-
+echo "Files downloaded!"
 echo
-echo "Anonymization complete!"
-echo "Run "bash deanonymizer.bash "$OUTPUTDIR"" when you are done grading!"
-echo "If students submit documents as pdfs, write your comments in a docx or doc"
-echo "file with exactly the same obfuscated name as the pdf in "$OUTPUTDIR". These"
-echo "comment files will then also be deanonymized at the same time!."
+
+# We now have two huge if blocks that I really should have properly
+# implemented with function calls. The first is for "all" mode, the
+# second is for "section" mode.
+if [[ "$USE" == "all" ]]; then
+   echo "Anonymizing all student assignments"
+   OUTPUTDIR="$CLASS-$ASSIGNMENT-anonymized"
+   rm $TEMPPROP   
+   mkdir -p "$OUTPUTDIR"
+   mv $TEMPDIR/* "$OUTPUTDIR"
+
+   echo
+   echo "Done!"
+   echo
+   echo "Run "bash deanonymizer.bash "$OUTPUTDIR"" when you are done grading!"
+   echo "If students submit documents as pdfs, write your comments in a doc(x)"
+   echo "file with exactly the same obfuscated name as the pdf in "
+   echo ""$OUTPUTDIR"."
+   echo "These comment files will then also be deanonymized at the same time!."
+elif [[ "$USE" == "section" ]]; then
+    echo "Anonymizing assignments section-by-section."
+    echo
+    echo "The script will now loop. On each loop it will ask you for a name for"
+    echo "each section and to upload a roster for that section. Typing 'done'"
+    echo "as the section name will terminate the loop."
+    echo
+    echo "Rosters are downloaded from classesv2 as xls files, but the script "
+    echo "requires that they be converted to csv (comma separated value)."
+    if command -v libreoffice > /dev/null; then
+	echo "Libreoffice found! anonymizer.bash will do the conversion for you."
+	LOFFICE=y
+    else
+	echo "Libreoffice not found. You will have to use Excel to manually export"
+	echo "the roster files from xls to csv."
+	LOFFICE=n
+    fi
+    echo
+
+    # We now begin the loop.
+    while :
+    do
+	echo "Please enter a name for this section. Type 'done' if you are finished."
+	read reply
+	if [[ "$reply" == "done" ]]; then
+	    break
+	else
+	    SECTION="$reply"
+	fi
+
+	echo "You now need to supply a roster file for this section."
+	echo "Go to "Roster" on Classesv2 and view the section you want."
+	echo "Then "export" the roster (upper-right-hand corner)"
+	if [[ "$LOFFICE" == "y" ]]; then
+	    echo "Please save the roster in the current directory under the name"
+	    echo ""$SECTION".xls."
+	    echo "Type 'ok' when the roster is saved here."
+	    read reply
+	    if [[ ! "$reply" == "ok" ]]; then
+		echo "Exiting! Please try again. Deleting student data"
+		rm -rf $TEMPDIR
+		exit 1
+	    fi	
+	    if [[ ! -f "$SECTION".xls ]]; then
+		echo "Roster file not found!"
+		echo "Exiting and deleting student data."
+		rm -rf $TEMPDIR
+		exit 2
+	    fi
+	    libreoffice --headless --convert-to csv "$SECTION".xls
+	    rm "$SECTION".xls
+	else
+	    echo "Please convert the roster yourself (e.g., with Excel) to csv"
+	    echo "and then save it in the current directory under the name"
+	    echo ""$SECTION".csv."
+	    echo "Type 'ok' when the roster is saved here."
+	    read reply 
+	    if [[ ! "$reply" == "ok" ]]; then
+		echo "Exiting! Please try again. Deleting student data"
+		rm -rf $TEMPDIR
+		exit 1
+	    fi	
+	    if [[ ! -f "$SECTION".csv ]]; then
+		echo "Roster file not found!"
+		echo "Exiting and deleting student data."
+		rm -rf $TEMPDIR
+		exit 2
+	    fi
+	fi
+
+	# We make the directory:
+	SECTIONDIR="$CLASS"-"$SECTION"-"$ASSIGNMENT"-anonymized
+	mkdir -p "$TEMPDIR"/"$SECTIONDIR"
+	# awk with csv:
+	# https://www.gnu.org/software/gawk/manual/gawk.html#Splitting-By-Content
+	# We use the grep to filter out the column names, and use the
+	# xargs so that the for loop is happy.
+	SIDS="$(grep "Student" "$SECTION".csv | awk -vFPAT="([^,]+)|(\"[^\"]+\")" '{print $2}' | xargs)"
+	# Here "$SIDS" can't be quoted, or for only loops a single
+	# time over the whole string.
+	for SID in $SIDS; do
+	    echo "$SID"
+	    # Here we ROT47 the name in accordance with the original.
+	    UNOBNAME="$CLASS-$ASSIGNMENT-$SID"
+	    echo "$UNOBNAME"
+	    OBNAME="$(echo "$UNOBNAME" | tr '\!-~' 'P-~\!-O')"
+	    echo "$OBNAME"
+	    cp $TEMPDIR/"$OBNAME"* $TEMPDIR/"$SECTIONDIR"
+	done
+
+	mv $TEMPDIR/"$SECTIONDIR" .
+	echo "Created "$SECTIONDIR"! Looping again..."
+	
+		
+    done
+
+    echo
+    echo "Section-by-section anonymization complete!"
+    echo "Created the following anonymized section directories:"
+    # We have to use this monstrosity because otherwise if you only
+    # anonymize one section, ls "$CLASS"- will list the contents of
+    # that directory.
+    ls | grep "$CLASS"-
+    echo
+    echo "We have not removed the roster csv files so you can use them"
+    echo "to deanonymize the section directories."
+    echo 
+    echo "Run 'bash deanonymizer.bash dirname' when you are done grading each!"
+    echo "If students submit documents as pdfs, write your comments in a doc(x)"
+    echo "file with exactly the same obfuscated name as the pdf in the same directory."
+    echo "These comment files will then also be deanonymized at the same time!."
+
+else
+    echo "Something has gone very wrong. Exiting."
+    # We must cleanup before we exit since student files are on the PC.
+    rm -rf "$TMPDIR"
+    exit 3
+fi
+
+# Clean up temp files at the end.
+rm -rf $TEMPDIR
